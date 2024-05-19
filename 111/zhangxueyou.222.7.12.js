bb844e9c0bc81750682d77f5158064192b8d2a0c
ciaooo55 <143322433+ciaooo55@users.noreply.github.com>
2024-05-20 03:07:39 +0800

    Update zhangxueyou.222.7.12.js

--- 111/zhangxueyou.222.7.12.js
+++ 111/zhangxueyou.222.7.12.js
@@ -1,4 +1,8 @@
-const response ={"code":200,"msg":"","data":[{"showId":1879517,"name":"2025555六19:00","performanceId":323031,"startTimeDateFormatted":"2024-06-15","startTimeWeekFormatted":"周六","startTimeTimeFormatted":"19:00","startTime":1700000000000,"endTime":1718456400000,"onSaleTime":0,"offSaleTime":0,"hasInventory":true,"showStatus":0,"showType":1,"showNote":"","areaUrl":null,"areaSvg":null,"areaSvgUrl":null,"showSeatType":0,"setExplain":"","showOrderLimitVO":{"maxBuyLimitPerOrder":4,"maxBuyLimitPerUser":4,"userAlreadyBuyNum":0,"userRemainBuyNum":4},"unusualStatus":0,"needFaceCheck":false,"seatMode":0,"hasDiscount":false,"minSellPrice":null,"preSelectVO":null,"soldOut":false,"normal":true,"default":false}],"paging":null,"attrMaps":{"serverTime":1715588112318},"success":true};
+/*
+ * 脚本功能：修改指定链接的响应体中的字段值
+ */
 
-const jsonResponse = JSON.stringify(response, null, 2);
-console.log(jsonResponse);
+
+const responseBody = {"code":200,"msg":"","data":[{"showId":1879517,"name":"加载成功","performanceId":323031,"startTimeDateFormatted":"2024-06-15","startTimeWeekFormatted":"周六","startTimeTimeFormatted":"19:00","startTime":1700000000000,"endTime":1718456400000,"onSaleTime":0,"offSaleTime":0,"hasInventory":true,"showStatus":0,"showType":1,"showNote":"","areaUrl":null,"areaSvg":null,"areaSvgUrl":null,"showSeatType":0,"setExplain":"","showOrderLimitVO":{"maxBuyLimitPerOrder":4,"maxBuyLimitPerUser":4,"userAlreadyBuyNum":0,"userRemainBuyNum":4},"unusualStatus":0,"needFaceCheck":false,"seatMode":0,"hasDiscount":false,"minSellPrice":null,"preSelectVO":null,"soldOut":false,"normal":true,"default":false}],"paging":null,"attrMaps":{"serverTime":1715588112318},"success":true};
+
+$done({body: JSON.stringify(responseBody)});
